345dd55109cf4d7c8fa48727b43ccd9fe5d9742e
James Baker <8171622+jmpb@users.noreply.github.com>
2021-08-19 12:26:45 +0100

    Stripped back for pentland stock checking

--- run_reports.py
+++ run_reports.py
@@ -1,14 +1,12 @@
+from datetime import datetime
 from pentland_stock_changes import PentlandStockChanges
-from rb_price_changes import RBPriceChanges
-from rb_stock_changes import RBStockChanges
 
 pentland_stock = PentlandStockChanges("Pentland")
-rb_prices = RBPriceChanges()
-rb_stock = RBStockChanges("RBStock")
 
-print("Checking RB Stock...")
-rb_stock.run()
-print("Checking RB Prices...")
-rb_prices.run()
+now = datetime.now()
+print("Starting check at", now.strftime("%d-%m-%Y %H:%M:%S"))
 print("Checking Pentland Stock...")
 pentland_stock.run()
+finished_time = datetime.now()
+print("Finished check at", finished_time.strftime("%d-%m-%Y %H:%M:%S"))
+print("----------------------------------------------------------------")
